d9caccb5a7960b7081d19c9200336ce84e1010d2
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-02-05 00:01:10 -0500

    commit tree f63ddc21b9935bd3d438db95d27806b778f13ba3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, include: :items
+        else
+            render json: { error: "User not found" }, status: :not_found
+        end
+    end
 end
